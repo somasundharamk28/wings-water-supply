13c651bb6266ea92baace970ed90d4d075bf4a29
somasundharamk <somasundharam@mittai.health>
2026-08-16 15:50:38 +0530

    admin api url added

--- frontend/src/AdminApp.jsx
+++ frontend/src/AdminApp.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import "./Admin.css";
 import SpeakButton from "./SpeakButton";
 
-const API_URL = "http://127.0.0.1:8000";
+const API_URL = import.meta.env.VITE_API_URL;
 
 function AdminApp() {
   const [token, setToken] = useState(
