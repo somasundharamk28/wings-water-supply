f68f133f7802bb6012640705f88b035ff953f18d
somasundharamk <somasundharam@mittai.health>
2026-08-16 15:31:12 +0530

    cord added again

--- backend/app/main.py
+++ backend/app/main.py
@@ -14,7 +14,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://wings-water-supply.vercel.app/"
+        "https://wings-water-supply.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
